6ed5a8603346cd629d5cb3a36bb3c2cb383e0cdb
shubhambangwal <jaisinghbhati900@gmail.com>
2025-01-20 16:12:03 +0530

    UI Part completed

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -36,15 +36,30 @@ const Navbar = () => {
               <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                 <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                   <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
-                  <p onClick={()=>navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                  <p onClick={() => navigate('/my-apponitments')} className='hover:text-black cursor-pointer'>My Appointments</p>
                   <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
                 </div>
               </div>
             </div> :
             <button className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block' onClick={() => navigate('/login')}>Create account</button>
         }
+        <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+        {/*  ------ Mobile Menu -------- */}
+        <div className={`md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all ${ShowMenu?"fixed w-full":"h-0 w-0"}`}>
+          <div className='flex items-center justify-between px-5 py-6'>
+            <img className='w-36' src={assets.logo} alt="" />
+            <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+          </div>
+          <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+            <NavLink onClick={()=>setShowMenu(false)} to="/"><p className='px-4 py-2 rounded full inline-block'>HOME</p></NavLink>
+            <NavLink onClick={()=>setShowMenu(false)} to="/doctors"><p className='px-4 py-2 rounded full inline-block'>ALL DOCTORS</p></NavLink>
+            <NavLink onClick={()=>setShowMenu(false)} to="/about"><p className='px-4 py-2 rounded full inline-block'>ABOUT</p></NavLink>
+            <NavLink onClick={()=>setShowMenu(false)} to="/contact"><p className='px-4 py-2 rounded full inline-block'>CONTACT</p></NavLink>
+          </ul>
+        </div>
       </div>
     </div>
+
   )
 }
 
--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+  const [userData, setUserData] = useState({
+    name: "Hartik Teetwal",
+    image: assets.profile_pic,
+    email: "teetwalhartik@gmail.com",
+    phone:"7505693449",
+    address: {
+      line1: "Muradnagar",
+      line2:"Ghaziabad"
+    },
+    gender: "Male",
+    dob:"2004-01-01"
+  })
+  const [isEdit,setIsEdit] = useState(false)
   return (
-    <div>
-      
+    <div className='max-w-lg flex flex-col gap-2 text-sm pt-5'>
+      <img className='w-36 rounded' src={userData.image} alt="" />
+      {
+        isEdit ? <input className='bg-gray-50 text-3xl font-medium max-w-60' type="text" onChange={(e) => setUserData(prev => ({ ...prev, name: e.target.value }))} value={userData.name} /> : <p className='font-medium text-3xl text-[#262626] mt-4'>{userData.name}</p>
+      }
+      <hr className='bg-[#ADADAD] h-[1px] border-none' />
+      <div>
+        <p className='text-[#797979] underline mt-3'>CONTACT INFORMATION</p>
+        <div className="grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-[#363636]">
+          <p className='font-medium'>Email id:</p>
+          <p className='text-blue-500'>{userData.email}</p>
+          <p className='font-medium'>Phone:</p>
+          {
+            isEdit ? <input className='bg-gray-50 max-w-52' type="text" onChange={(e) => setUserData(prev => ({ ...prev, name: e.target.value }))} value={userData.phone} /> : <p className='text-blue-500'>{userData.phone}</p>
+          }
+          <p className='font-medium'>Address:</p>
+          {
+            isEdit ? <p>
+              <input className='bg-gray-50' type="text" onChange={(e)=>setUserData(prev=>({...prev,address:{...prev.address,line1:e.target.value}}))} value={userData.address.line1}/>
+              <br />
+              <input className='bg-gray-50' type="text" onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line2: e.target.value } }))} value={userData.address.line2} />
+            </p> : <p className='text-gray-500'>
+              {userData.address.line1}
+              <br />
+              {userData.address.line2}                
+            </p>
+          }
+        </div>
+      </div>
+      <div>
+        <p className='text-[#797979] underline mt-3'>BASIC INFORMATION</p>
+        <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-gray-600'>
+          <p className='font-medium'>Gender:</p>
+          {
+            isEdit ? <select className='max-w-20 bg-gray-50' onChange={(e)=>setUserData(prev=>({...prev,gender:e.target.value}))} value={userData.gender}>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select> : <p className='text-gray-500'>{userData.gender}</p>
+          }
+          <p className='font-medium'>Birthday</p>
+          {
+            isEdit ? <input className='max-w-28 bg-gray-50' type="date" onChange={(e) => setUserData(prev => ({ ...prev, dob: e.target.value }))} value={userData.dob} /> : <p className='text-gray-500'>{userData.dob}</p>
+          }
+        </div>
+      </div>
+      <div className='mt-10'>
+        {
+          isEdit ?
+            <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(false)}>Save information</button> :
+            <button className='border border-primary px-8 py-2 rounded-full hover:bg-primary hover:text-white transition-all' onClick={()=>setIsEdit(true)}>Edit</button>
+        }
+      </div>
+
     </div>
   )
 }
